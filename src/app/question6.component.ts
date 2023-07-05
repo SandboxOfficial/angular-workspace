c1f1b3209acf6db2571fd60cc8887864de328299
SandboxOfficial <138592857+SandboxOfficial@users.noreply.github.com>
2023-07-05 14:51:37 +0100

    1 file modified

--- src/app/question6.component.ts
+++ src/app/question6.component.ts
@@ -1,7 +1,7 @@
 /**QUESTION: How to implement Typeahead search text box which triggers subscription after 3char keyed. Also make sure canceling effects and avoiding nested Subscriptions **/
 
 import { Component } from '@angular/core';
-import { debounceTime, map, Observable, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { User, TesteventService } from './testEvent.service';
 
 @Component({
@@ -10,27 +10,33 @@ import { User, TesteventService } from './testEvent.service';
   <ng-template #rt let-r="result" let-t="term">
   <ngb-highlight [result]="r.name" [term]="t"></ngb-highlight>
   </ng-template>
-  <label for="typeahead-template">Search user:</label> 
+  <label for="typeahead-template">Search user:</label> {{hint}} 
   <input id="typeahead-template" type="text" class="form-control"[(ngModel)]="model" #ctrl="ngModel"  [ngbTypeahead]="search" [resultTemplate]="rt"
     [inputFormatter]="formatter" />
+    <p>Keyup regular : {{typeterm}}</p>
+    <p>Keyup Delayed : {{typeterm}}</p>
   `,
   providers: [TesteventService],
 })
 export class Question6Component {
   public model: any;
+  public hint: any;
   public typeterm: any;
   constructor(private testService: TesteventService) {}
 
   search = (text$: Observable<string>) =>
     text$.pipe(
-      debounceTime(1000),
-      map((term: string) =>
-        term.length > 2
-          ? this.testService.users.filter((user: User) =>
-              user.name.includes(term)
-            )
-          : []
-      )
+      map((term: string) => {
+        this.typeterm = term;
+        if (term.length > 2) {
+          return this.testService.users.filter((user: User) =>
+            user.name.includes(term)
+          );
+        } else {
+          this.hint = 'Type atleast 3char';
+          return [];
+        }
+      })
     );
 
   formatter = (x: { name: string }) => x.name;
